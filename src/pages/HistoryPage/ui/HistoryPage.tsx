891206d8d4cecc3c2e8e01fb196f0205ff9e6c3f
Barklim <46284548+Barklim@users.noreply.github.c>
2023-12-13 05:31:32 +0800

    fix: showcase page descriptions

--- src/pages/HistoryPage/ui/HistoryPage.tsx
+++ src/pages/HistoryPage/ui/HistoryPage.tsx
@@ -14,7 +14,8 @@ import {
     mockTitle3,
     mockTitle4,
     mockTitle5,
-    mockTitle6
+    mockTitle6,
+    mockTitleTitles,
 } from '@/shared/const/mock';
 
 const HistoryPage = () => {
@@ -32,42 +33,70 @@ const HistoryPage = () => {
                     <Circle id='5item_90rad' pointCount={5} titles={mockTitle5}/>
                     <Circle id='6item_90rad' pointCount={6} titles={mockTitle6}/>
                 </HStack>
-                <div style={{width: '100%'}}>
-                    <Code text={mockTitleCountPoints} />
-                </div>
+                <VStack gap='8' max>
+                    <Text size={'m'} selectNone text={t('title points subtext')}></Text>
+                    <div style={{width: '100%'}}>
+                        <Code text={mockTitleCountPoints} />
+                    </div>
+                </VStack>
 
                 <Text size={'m'} selectNone title={t('title visibility')}></Text>
                 <HStack gap='32'>
+                    <Circle id='6item_angles' pointCount={6} numberVisibility={true}/>
                     <Circle id='2item_angles' pointCount={2} numberVisibility={true} extraRotation={0}/>
                     <Circle id='3item_angles' pointCount={3} numberVisibility={true} extraRotation={45}/>
                     <Circle id='4item_angles' pointCount={4} numberVisibility={true} extraRotation={89}/>
                     <Circle id='5item_angles' pointCount={5} numberVisibility={true} extraRotation={15}/>
-                    <Circle id='6item_angles' pointCount={6} numberVisibility={true}/>
                 </HStack>
-                <div style={{width: '100%'}}>
-                    <Code text={mockTitleVisibilityNumbers} />
-                </div>
+                <VStack gap='8' max>
+                    <Text size={'m'} selectNone text={t('title visibility subtext')}></Text>
+                    <div style={{width: '100%'}}>
+                        <Code text={mockTitleVisibilityNumbers} />
+                    </div>
+                </VStack>
 
                 <Text size={'m'} selectNone title={t('title duration')}></Text>
                 <HStack gap='32'>
-                    <Circle id='2item_duration04' pointCount={2} numberVisibility={true} duration={0.4} titles={mockTitle2} />
-                    <Circle id='6item_duration04' pointCount={6} numberVisibility={true} duration={0.4} titles={mockTitle6}/>
-                    <Circle id='2item_duration2' pointCount={2} numberVisibility={true} duration={2} titles={mockTitle2}/>
-                    <Circle id='6item_duration2' pointCount={6} numberVisibility={true} duration={2} titles={mockTitle6}/>
-                    <Circle id='6item_duration' pointCount={6} numberVisibility={true} titles={mockTitle6} />
+                    <VStack>
+                        <Circle id='6item_duration' radius={150} pointCount={6} numberVisibility={true} titles={mockTitle6} />
+                        <HStack gap='32'>
+                            <Circle id='2item_duration04' radius={110} pointCount={2} numberVisibility={true} duration={0.4} titles={mockTitle2}/>
+                            <Circle id='6item_duration04' radius={110} pointCount={6} numberVisibility={true} duration={0.4} titles={mockTitle6}/>
+                            <Circle id='2item_duration2' radius={110} pointCount={2} numberVisibility={true} duration={2} titles={mockTitle2}/>
+                            <Circle id='6item_duration2' radius={110} pointCount={6} numberVisibility={true} duration={2} titles={mockTitle6}/>
+                        </HStack>
+                    </VStack>
                 </HStack>
-                <div style={{width: '100%'}}>
-                    <Code text={mockTitleAnimationDuration} />
-                </div>
+                <VStack gap='8' max>
+                    <Text size={'m'} selectNone text={t('title duration subtext')}></Text>
+                    <div style={{width: '100%'}}>
+                        <Code text={mockTitleAnimationDuration} />
+                    </div>
+                </VStack>
 
                 <Text size={'m'} selectNone title={t('title radius')}></Text>
                 <HStack gap='32'>
                     <Circle id='5item_175rad' pointCount={5} radius={175} titles={mockTitle5}/>
                     <Circle id='3item_50rad' pointCount={2} radius={50} titles={mockTitle2}/>
                 </HStack>
-                <div style={{width: '100%'}}>
-                    <Code text={mockTitleRadius} />
-                </div>
+                <VStack gap='8' max>
+                    <Text size={'m'} selectNone text={t('title radius subtext')}></Text>
+                    <div style={{width: '100%'}}>
+                        <Code text={mockTitleRadius} />
+                    </div>
+                </VStack>
+
+                <Text size={'m'} selectNone title={t('title titles')}></Text>
+                <HStack gap='32'>
+                    <Circle id='4item_title' radius={120} pointCount={4}/>
+                    <Circle id='6item_title' radius={120} pointCount={6} titles={['you so', 'f@ckin', 'precious', 'when you', '❤️❤ smile ❤❤️️', '']}/>
+                </HStack>
+                <VStack gap='8' max>
+                    <Text size={'m'} selectNone text={t('title titles subtext')}></Text>
+                    <div style={{width: '100%'}}>
+                        <Code text={mockTitleTitles} />
+                    </div>
+                </VStack>
             </VStack>
         </VStack>
     </Page>;
--- src/shared/const/mock.ts
+++ src/shared/const/mock.ts
@@ -4,53 +4,52 @@ export const mockTitle4 = ['Title1','Title2','Title3', 'Title4',];
 export const mockTitle5 = ['Title1','Title2','Title3', 'Title4','Title5'];
 export const mockTitle6 = ['Title1','Title2','Title3', 'Title4','Title5','Title6'];
 
-export const mockTitleCountPoints = '<HStack gap=\'32\'>\n' +
-    '  <Circle id=\'2item_90rad\' pointCount={2}/>' +
+export const mockTitleCountPoints =
+    '<Circle pointCount={2}/>' +
     '\n' +
-    '  <Circle id=\'3item_90rad\' pointCount={3}/>' +
+    '<Circle pointCount={3}/>' +
     '\n' +
-    '  <Circle id=\'4item_90rad\' pointCount={4}/>' +
+    '<Circle pointCount={4}/>' +
     '\n' +
-    '  <Circle id=\'5item_90rad\' pointCount={5}/>' +
+    '<Circle pointCount={5}/>' +
     '\n' +
-    '  <Circle id=\'6item_90rad\' pointCount={6}/>' +
-    '\n' +
-    '</HStack>'
+    '<Circle pointCount={6}/>'
 
-export const mockTitleVisibilityNumbers = '<HStack gap=\'32\'>\n' +
-    '  <Circle id=\'2item_angles\' pointCount={2}/> numberVisibility={true} extraRotation={0}' +
+export const mockTitleVisibilityNumbers =
+    '<Circle numberVisibility={true}/>' +
     '\n' +
-    '  <Circle id=\'3item_angles\' pointCount={3}/> numberVisibility={true} extraRotation={45}' +
+    '<Circle numberVisibility={true} extraRotation={0}/>' +
     '\n' +
-    '  <Circle id=\'4item_angles\' pointCount={4}/> numberVisibility={true} extraRotation={89}' +
+    '<Circle numberVisibility={true} extraRotation={45}/>' +
     '\n' +
-    '  <Circle id=\'5item_angles\' pointCount={5}/> numberVisibility={true} extraRotation={15}' +
+    '<Circle numberVisibility={true} extraRotation={89}/>' +
     '\n' +
-    '  <Circle id=\'6item_angles\' pointCount={6}/> numberVisibility={true}' +
-    '\n' +
-    '  {/*45° angles by default*/}' +
-    '\n' +
-    '</HStack>'
+    '<Circle numberVisibility={true} extraRotation={15}/>'
 
-export const mockTitleAnimationDuration = '<HStack gap=\'32\'>\n' +
-    '  <Circle id=\'2item_duration04\' pointCount={2}/> numberVisibility={true} duration={0.4}' +
+export const mockTitleAnimationDuration =
+    '<Circle pointCount={6} numberVisibility={true}' +
     '\n' +
-    '  <Circle id=\'6item_duration04\' pointCount={6}/> numberVisibility={true} duration={0.4}' +
+    '<Circle pointCount={2} numberVisibility={true} duration={0.4}/>' +
     '\n' +
-    '  <Circle id=\'2item_duration2\' pointCount={2}/> numberVisibility={true} duration={2}' +
+    '<Circle pointCount={6} numberVisibility={true} duration={0.4}/>' +
     '\n' +
-    '  <Circle id=\'6item_duration2\' pointCount={6}/> numberVisibility={true} duration={2}' +
+    '<Circle pointCount={2} numberVisibility={true} duration={2}/>' +
     '\n' +
-    '  <Circle id=\'6item_duration\' pointCount={6}/> numberVisibility={true}' +
-    '\n' +
-    '  {/*Step animation duration by default is 0.8*/}' +
-    '\n' +
-    '</HStack>'
+    '<Circle pointCount={6} numberVisibility={true} duration={2}/>'
 
-export const mockTitleRadius = '<HStack gap=\'32\'>\n' +
-    '  <Circle id=\'5item_175rad\' pointCount={5} radius={175}/>' +
+export const mockTitleRadius =
+    '<Circle pointCount={5} radius={175}/>' +
     '\n' +
-    '  <Circle id=\'3item_50rad\' pointCount={2} radius={50}/>' +
+    '<Circle pointCount={2} radius={50}/>'
+
+export const mockTitleTitles =
+    '<Circle pointCount={4} />' +
     '\n' +
-    '</HStack>'
+    '<Circle' +
+    '\n' +
+    '  pointCount={6}' +
+    '\n' +
+    '  titles={[\'you so\', \'f@ckin\', \'precious\', \'when you\', \'❤️❤ smile ❤❤️️\', \'\']}/>' +
+    '\n' +
+    '/>'
 
